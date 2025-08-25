dff11d2a5bd707d055a8e2975de13c3b1bfe1364
RXstoinkboy <erykludwin@gmail.com>
2025-08-26 00:37:36 +0200

    can now set dimensions for each layer separately

--- app/(app)/(home)/cake-recipe-card.tsx
+++ app/(app)/(home)/cake-recipe-card.tsx
@@ -9,6 +9,9 @@ import { Cake } from "@/app/(app)/api/cakes/actions";
 import { Recipe } from "@/app/(app)/api/recipes/actions";
 import { RemoveCake } from "./modals/delete";
 import { EditCake } from "./modals/edit";
+import { Separator } from "@/components/ui/separator";
+
+const createKey = (id: string, index: number) => `${id}-${index}`;
 
 type RecipeCardProps = {
   cake: Cake;
@@ -16,9 +19,24 @@ type RecipeCardProps = {
 };
 
 export const RecipeCard = ({ cake, recipes }: RecipeCardProps) => {
-  const [value, setValue] = useState([18]);
-  const onChange = (newValue: number[]) => {
-    setValue(newValue);
+  const orderedRecipes = cake.cake_recipes.sort((a, b) => a.order - b.order);
+  const initialDimensions = orderedRecipes.reduce(
+    (acc, { recipe_id }, index) => {
+      const key = createKey(recipe_id.id, index);
+      return {
+        ...acc,
+        [key]: [18],
+      };
+    },
+    {},
+  );
+  const [dimensions, setDimensions] = useState(initialDimensions);
+
+  const onChange = (newValue: number[], id: string) => {
+    setDimensions((prevDimensions) => ({
+      ...prevDimensions,
+      [id]: newValue,
+    }));
   };
 
   const totalPriceForDefaultDiameter = cake.cake_recipes.reduce(
@@ -35,7 +53,12 @@ export const RecipeCard = ({ cake, recipes }: RecipeCardProps) => {
       (Math.PI * diameter ** 2) / 4 / ((Math.PI * defaultDiameter ** 2) / 4);
     return totalPriceForDefaultDiameter * priceRatio;
   };
-  const priceForDiameter = calculatePriceForDiameter(value[0]);
+  const finalPrice = (Object.values(dimensions) as number[][]).reduce(
+    (acc, item) => {
+      return acc + calculatePriceForDiameter(item[0]);
+    },
+    0,
+  );
 
   return (
     <Card className="min-w-full max-w-sm mb-2 ">
@@ -53,33 +76,53 @@ export const RecipeCard = ({ cake, recipes }: RecipeCardProps) => {
             </Button>
           </RemoveCake>
         </CardTitle>
-        <CardContent className="grid grid-cols-2 px-0 gap-8">
-          <ul>
-            {cake.cake_recipes
-              .sort((a, b) => a.order - b.order)
-              .map(({ recipe_id }, index) => (
-                <li key={`${recipe_id.id}-${index}`}>{recipe_id.name}</li>
-              ))}
+        <CardContent className="p-0 mt-4">
+          <ul className="grid gap-2 px-1">
+            {orderedRecipes.map(({ recipe_id }, index) => {
+              const key = createKey(
+                recipe_id.id,
+                index,
+              ) as keyof typeof dimensions;
+              return (
+                <li
+                  key={key}
+                  className="grid grid-cols-1 md:grid-cols-2 p-2 gap-8 border rounded-md items-center"
+                >
+                  <p className="text-lg font-semibold">{recipe_id.name}</p>
+                  <section className="flex items-center justify-center gap-3">
+                    <section className="flex flex-1 flex-col">
+                      <Slider
+                        min={10}
+                        max={32}
+                        step={1}
+                        value={dimensions[key]}
+                        onValueChange={(newValue) => onChange(newValue, key)}
+                      />
+                    </section>
+                    <article className="flex gap-2 items-center">
+                      <article className=" flex gap-1 w-full justify-center items-center">
+                        <CircleOff size={18} className="rotate-90" />
+                        <span>{dimensions[key][0]} cm</span>
+                      </article>{" "}
+                      /
+                      <p>
+                        {calculatePriceForDiameter(dimensions[key][0]).toFixed(
+                          2,
+                        )}
+                        <span className="ml-1">PLN</span>
+                      </p>
+                    </article>
+                  </section>
+                </li>
+              );
+            })}
           </ul>
-          <section className="flex-1 flex flex-col items-end justify-center gap-3">
-            <article className="flex gap-1 items-end">
-              <h2 className="text-6xl font-bold">
-                {priceForDiameter.toFixed(2)}
-              </h2>
-              <h3 className="text-2xl font-semibold">PLN</h3>
-            </article>
-            <Slider
-              min={10}
-              max={32}
-              step={1}
-              value={value}
-              onValueChange={onChange}
-            />
-            <article className="flex gap-1 w-full justify-center items-center">
-              <CircleOff size={18} className="rotate-90" />
-              <span>{value[0]} cm</span>
-            </article>
-          </section>
+          <Separator className="my-2" />
+          <article className="flex justify-end items-baseline gap-2 px-1">
+            <h3 className="text-2xl mr-2">Suma:</h3>
+            <h2 className="text-6xl font-bold">{finalPrice.toFixed(2)}</h2>
+            <h3 className="text-2xl font-semibold">PLN</h3>
+          </article>
         </CardContent>
       </CardHeader>
     </Card>
